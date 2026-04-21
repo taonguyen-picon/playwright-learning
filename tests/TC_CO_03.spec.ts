dd150359f788d44a27b125fdce30c9b15a7fc273
Nguyen Van Tao <taonv@DD-TaoNV.local>
2026-04-22 00:30:31 +0700

    Upload TC_CO_03 ver 1.0

--- tests/TC_CO_03.spec.ts
+++ tests/TC_CO_03.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, Locator } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Verify that the cart information remains consistent from the Cart page to the Overview page', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
@@ -14,20 +14,6 @@ test('test', async ({ page }) => {
   await page.locator('[data-test="shopping-cart-link"]').click();
 
   const productCartItems = page.locator('[data-test="inventory-item"]');
-  // const cartItemCount = await productCartItems.count();
-  // const cartdata = [];
-
-  // Extract and log the cart data
-  // for(let i = 0 ; i < cartItemCount; i++) {
-  //   const item = productCartItems.nth(i);
-
-  //   // Extract the name, price, and quantity for each item in the cart and store it in the cartdata array
-  //   cartdata.push({
-  //     name: (await item.locator('[data-test="inventory-item-name"]').textContent())?.trim(),
-  //     price: (await item.locator('[data-test="inventory-item-price"]').textContent())?.trim(),
-  //     quantity: (await item.locator('[data-test="item-quantity"]').textContent())?.trim()
-  //   });
-  // }
 
   // Function to extract item data from a given locator
   const getItemsData = async (items : Locator) => {
@@ -47,8 +33,7 @@ test('test', async ({ page }) => {
   return data;
 };
 
-const cartdata = await getItemsData(productCartItems);
-
+  const cartdata = await getItemsData(productCartItems);
 
   // Proceed to checkout
   await page.locator('[data-test="checkout"]').click();
@@ -60,23 +45,9 @@ const cartdata = await getItemsData(productCartItems);
   await page.locator('[data-test="postalCode"]').fill('67890');
   await page.locator('[data-test="continue"]').click();
 
+  // Extract checkout data
   const productCheckoutItems = page.locator('[data-test="inventory-item"]');
-
   const checkoutData = await getItemsData(productCheckoutItems);
-  // const checkoutItemCount = await productCheckoutItems.count();
-  // const checkoutData = [];
-
-  // // Extract and log the checkout data
-  // for (let i = 0; i < checkoutItemCount; i ++) {
-  //   const item = productCheckoutItems.nth(i);
-
-  //   // Extract the name, price, and quantity for each item in the checkout page and store it in the checkoutData array
-  //   checkoutData.push({
-  //     name: (await item.locator('[data-test="inventory-item-name"]').textContent())?.trim(),
-  //     price: (await item.locator('[data-test="inventory-item-price"]').textContent())?.trim(),
-  //     quantity: (await item.locator('[data-test="item-quantity"]').textContent())?.trim()
-  //   })
-  // }
 
   // Validate that the cart data matches the checkout data
   expect(cartdata).toEqual(checkoutData);
